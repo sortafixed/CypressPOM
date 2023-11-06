e97e969ed95831577dbb339d4399c7709c24248f
sortafixed <128575362+sortafixed@users.noreply.github.com>
2023-11-05 22:45:36 -0600

    formatted code

--- cypress/e2e/login_logout.cy.js
+++ cypress/e2e/login_logout.cy.js
@@ -1,28 +1,26 @@
 /// <reference types="cypress" />
-import { LoginPage } from "../pages/login_page"
-import { SiteHeader } from "../pages/site_header"
+import { LoginPage } from "../pages/login_page";
+import { SiteHeader } from "../pages/site_header";
 
-context('Testing Login Page', () => {
-    beforeEach(() => {
-        cy.visit('https://dashboard.foreman.mn/login/')
-    })
+context("Testing Login Page", () => {
+  beforeEach(() => {
+    cy.visit("https://dashboard.foreman.mn/login/");
+  });
 
+  const loginPage = new LoginPage();
+  const siteHeader = new SiteHeader();
 
-let loginPage = new LoginPage()
-let siteHeader = new SiteHeader()
-
-it('login/logout test', () =>{
-
-    cy.fixture('testdata').then(({ email, password}) => {  
-    loginPage.assertLoginLogo();
-    loginPage.assertUsernameFieldText();
-    loginPage.enterUsername(email);
-    loginPage.assertPasswordFieldText()
-    loginPage.enterPassword(password);
-    loginPage.assertLoginBtnText();
-    loginPage.clickSignInBtn();
-    siteHeader.logout();
-    loginPage.assertLoginBtnText(); 
-    })
-  })
-})
+  it("login/logout test", () => {
+    cy.fixture("testdata").then(({ email, password }) => {
+      loginPage.assertLoginLogo();
+      loginPage.assertUsernameFieldText();
+      loginPage.enterUsername(email);
+      loginPage.assertPasswordFieldText();
+      loginPage.enterPassword(password);
+      loginPage.assertLoginBtnText();
+      loginPage.clickSignInBtn();
+      siteHeader.logout();
+      loginPage.assertLoginBtnText();
+    });
+  });
+});
